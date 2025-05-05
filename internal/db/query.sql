3eef65d41bf0f9bd9479a89412a2ec6a975053c3
30Piraten <victorehikioyaraeva@outlook.com>
2025-05-05 20:54:32 +0100

    Added schema + query for user profile. Generated go code with sqlc

--- internal/db/query.sql
+++ internal/db/query.sql
@@ -1,11 +1,11 @@
 --- name: GetUser :one 
-SELECT * FROM users WHERE id = $1;
+SELECT id, name, email, created_at FROM users WHERE id = $1;
 
 -- name: ListAllUsers :many
-SELECT * FROM users;
+SELECT id, name, email, created_at FROM users;
 
 -- name: CreateUser :one
 INSERT INTO users (id, name, email, created_at)
 VALUES ($1, $2, $3, $4)
 
-RETURNING *;
+RETURNING id, name, email, created_at;
