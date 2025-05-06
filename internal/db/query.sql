0b8e69ba572ae2143102844bb51de7762a46f91d
30Piraten <victorehikioyaraeva@outlook.com>
2025-05-06 13:48:09 +0100

    Updated GetUser

--- internal/db/query.sql
+++ internal/db/query.sql
@@ -1,11 +1,11 @@
---- name: GetUser :one 
-SELECT id, name, email, created_at FROM users WHERE id = $1;
-
--- name: ListAllUsers :many
-SELECT id, name, email, created_at FROM users;
-
 -- name: CreateUser :one
 INSERT INTO users (id, name, email, created_at)
 VALUES ($1, $2, $3, $4)
 
-RETURNING id, name, email, created_at;
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: ListAllUsers :many
+SELECT * FROM users;
